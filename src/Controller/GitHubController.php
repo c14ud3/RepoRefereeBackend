7eead06d3a110900ecf0bd55cccbf715d09159c5
Claudio <work@fleisch-mann.ch>
2025-04-10 13:35:45 +0200

    Test Github Webhook

---
+++ src/Controller/GitHubController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class GitHubController extends AbstractController
+{
+    #[Route('/github/hook/{auth}', name: 'app_git_hub')]
+    public function index($auth): Response
+    {
+		// TODO: Check authentication
+
+		// * Load request data
+		$REQUESTDATA = json_decode(file_get_contents('php://input') ?? '{}', true) ?? [];
+
+		return new Response(print_r($REQUESTDATA, true));
+    }
+}
