ff645b19d85675eb20984f7d43020e04f2114123
Claudio <work@fleisch-mann.ch>
2025-04-10 14:07:01 +0200

    Test Github Webhook

--- src/Controller/GitHubController.php
+++ src/Controller/GitHubController.php
@@ -15,11 +15,41 @@ final class GitHubController extends AbstractController
 
 		// * Load request data
 		$REQUESTDATA = json_decode(file_get_contents('php://input') ?? '{}', true) ?? [];
+		$githubEvent = $_SERVER['HTTP_X_GITHUB_EVENT'] ?? '';
+		$githubAction = $REQUESTDATA['action'] ?? '';
 
-		// if(!isset($REQUESTDATA['action']) || !in_array($REQUESTDATA['action'], ['opened', 'created']) ||
-		// 	!isset($REQUESTDATA['comment']) || empty($REQUESTDATA['comment']))
-		// 	return new Response('Nothing to do.');
+		// * Check if we even have anything to do
+		if(!(
+			($githubEvent == 'issues' && $githubAction == 'opened') ||
+			($githubEvent == 'issues' && $githubAction == 'edited') ||
+			($githubEvent == 'issue_comment' && $githubAction == 'created') ||
+			($githubEvent == 'issue_comment' && $githubAction == 'edited')
+		)) {
+			return new Response('Nothing to do.');
+		}
 
-		return new Response();
+		if(isset($REQUESTDATA['comment'])) {
+			// dealing with an issue_comment
+			$url = $REQUESTDATA['comment']['html_url'] ?? '';
+			$title = $REQUESTDATA['issue']['title'] ?? '';
+			$contextComments = [];
+			$comment = $REQUESTDATA['comment']['body'] ?? '';
+		} else if(isset($REQUESTDATA['issue'])) {
+			// ! 'issue' is set aswell in 'issue_comment'
+			// dealing with an issue
+			$url = $REQUESTDATA['issue']['html_url'] ?? '';
+			$title = $REQUESTDATA['issue']['title'] ?? '';
+			$contextComments = [];
+			$comment = $REQUESTDATA['issue']['body'] ?? '';
+		} else {
+			return new Response('Corresponding data not found in request.', 400);
+		}
+
+		return new Response(print_r([
+			'url' => $url,
+			'title' => $title,
+			'contextComments' => $contextComments,
+			'comment' => $comment,
+		], true));
     }
 }
